117cdf499a77edc854ca84baa43fa845eedec005
vijay.katuri <vijay.katuri@acquia.com>
2020-01-11 06:25:38 -0500

    first commit

---
+++ modules/networking/vpc/vpc-outputs.tf
@@ -0,0 +1,8 @@
+output "vpc_id" {
+value = "${aws_vpc.vpc.id}"
+
+}
+
+#output "route_table_id"{
+#value = "${aws_vpc.main_route_table_id}"
+#}
---
+++ modules/networking/vpc/vpc-variables.tf
@@ -0,0 +1,6 @@
+variable "cidr"{
+  type = string
+  
+}
+
+
