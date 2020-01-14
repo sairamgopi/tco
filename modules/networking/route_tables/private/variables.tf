4dac32a08c7d8bd556e17edaafbb14e12249adfb
farhan0041 <farhan.ahemad@itp-inc.com>
2020-01-14 12:56:04 -0500

    rt

---
+++ modules/networking/route_tables/private/variables.tf
@@ -0,0 +1,25 @@
+variable "vpc_id"{
+type = string
+}
+
+
+
+variable "AZs" {
+type = list(string)
+deafult = ["a","b","c","d","e","f"]
+}
+
+variable "region" {
+type = string
+}
+
+
+
+variable "routetype"{
+type =  string
+default = "private"
+}
+
+variable "env"{
+type = string
+}
