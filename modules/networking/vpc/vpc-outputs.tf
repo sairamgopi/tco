a5d8d701ef27e6d58fd164930c232ac1da8cf350
vijay.katuri <vijay.katuri@acquia.com>
2020-01-15 23:46:33 -0500

    vijay|vpc modules linted

--- modules/networking/vpc/vpc-outputs.tf
+++ modules/networking/vpc/vpc-outputs.tf
@@ -1,8 +1,6 @@
 output "vpc_id" {
-value = "${aws_vpc.vpc.id}"
+
+  value = "${aws_vpc.vpc.id}"
 
 }
 
-#output "route_table_id"{
-#value = "${aws_vpc.main_route_table_id}"
-#}
--- modules/networking/vpc/vpc-variables.tf
+++ modules/networking/vpc/vpc-variables.tf
@@ -1,4 +1,5 @@
 variable "cidr"{
+
   type = string
   
 }
